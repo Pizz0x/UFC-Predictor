f7bb7820027dcb6179937aad73a922f51b643c36
Pizz0x <filippopizzo03@gmail.com>
2025-06-17 21:37:22 +0200

    first commit

---
+++ RF_model.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+df_event = pd.read_csv("data/ufc_event_data.csv")
+df_fight = pd.read_csv("data/ufc_fight_data.csv")
+df_fight_stat = pd.read_csv("data/ufc_fight_stat_data.csv")
+df_fighter = pd.read_csv("data/ufc_fighter_data.csv")
+
+# first thing I will do is merge the dataframe in one big dataframe with all the informations
