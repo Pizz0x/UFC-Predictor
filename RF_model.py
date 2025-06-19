97f467b7f6833bde0cbd5eb69c3ed12bc2f5fc1e
Pizz0x <filippopizzo03@gmail.com>
2025-06-19 14:45:54 +0200

    data cleaning of fight dataset

--- RF_model.py
+++ RF_model.py
@@ -29,5 +29,44 @@ oh = OneHotEncoder(sparse_output=False)
 oh.fit(categorical_fighter)
 encoded = oh.transform(categorical_fighter)
 encoded_df = pd.DataFrame(encoded, columns=oh.get_feature_names_out(), index=numerical_fighter.index)
-numerical_fighter = pd.concat([numerical_fighter, encoded_df], axis=1)
-print(numerical_fighter.head(10))
+df_fighter = pd.concat([numerical_fighter, encoded_df], axis=1)
+print(df_fighter.isna().values.any())
+#print(numerical_fighter.head(10))
+
+# 1.2 FIGHT
+df_fight["gender"] = df_fight["gender"].map({'M': 0, 'F': 1})
+df_fight["title_fight"] = df_fight["title_fight"].map({'F': 0, 'T': 1})
+df_fight["loser"] = df_fight.apply(lambda row: row['f_2'] if row['winner'] == row ['f_1'] else row['f_1'], axis = 1)
+def time_to_seconds(t):
+    minutes, seconds = map(int, t.split(':'))
+    return minutes * 60 + seconds
+
+df_fight["finish_time"] = df_fight["finish_time"].apply(time_to_seconds)
+df_fight["fight_duration"] = 300 * (df_fight["finish_round"] - 1) + df_fight["finish_time"]
+
+#df_fight["result_details"].fillna(df_fight["result"], inplace=True)
+
+def detailed_decision(row):
+    if row['result'] == 'Decision':
+        return row['result_details'] + ' ' + row['result']
+    return row['result']
+    
+df_fight['result'] = df_fight.apply(detailed_decision, axis=1)
+
+
+df_fight["weight_class"].fillna('Catch Weight', inplace=True)
+
+df_fight.drop(["fight_url", "finish_time", "finish_round", "f_1", "f_2", "referee", "result_details"], axis=1, inplace=True)
+df_fight.dropna(subset=['winner', 'loser'], inplace=True)
+print(df_fight.isna().values.any())
+
+# One-Hot Encoding
+is_numerical = np.array([np.issubdtype(dtype, np.number) for dtype in df_fight.dtypes])  
+categorical_idx = np.flatnonzero(is_numerical==False)
+categorical_fight = df_fight.iloc[:, categorical_idx]
+oh.fit(categorical_fight)
+encoded = oh.transform(categorical_fight)
+numerical_idx = np.flatnonzero(is_numerical)
+numerical_fight = df_fight.iloc[:, numerical_idx]
+encoded_df = pd.DataFrame(encoded, columns=oh.get_feature_names_out(), index=numerical_fight.index)
+df_fight = pd.concat([numerical_fight, encoded_df], axis=1)
