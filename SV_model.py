0031da61b7799fb9f3be0c95b5d7deb04dc76a52
Pizz0x <filippopizzo03@gmail.com>
2025-06-26 12:06:56 +0200

    started working on the Support Vector Model

--- SV_model.py
+++ SV_model.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
 df_event = pd.read_csv("data/ufc_event_data.csv")
@@ -248,28 +248,22 @@ y_train = df_train["winner"]
 X_pred = df_pred.drop(columns=["winner"])
 y_pred = df_pred["winner"]
 
-#print(X.head())
-# In this file we will use the Random Forest as classifier since it's an easy to implement and robust model
-base_model = RandomForestClassifier()
-parameters = { 'n_estimators': [50, 100],
-    'max_leaf_nodes': [50, 100],
-    'max_depth': [10, 20, None],
-    'min_samples_split': [5, 10],
-    'bootstrap': [True, False]
-    }
-tuned_model = GridSearchCV(base_model, parameters, cv=5, scoring='accuracy', n_jobs=-1)
+# This is the hyper-parameter tuning since we only have 1 significative parameter
+model = SVC()
+parameters = { 'C': [0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]}
+tuned_model = GridSearchCV(model, parameters, cv=5, verbose=0)
 tuned_model.fit(X_train, y_train)
+
 print ("Best Score: {:.3f}".format(tuned_model.best_score_) )
-#print("Best Params: ", tuned_model.best_params_)
 test_acc = accuracy_score(y_true = y_pred, y_pred = tuned_model.predict(X_pred) )
 print("Test Accuracy: {:.3f}".format(test_acc) )
 
-for feature, importance in zip(X_train.columns, tuned_model.best_estimator_.feature_importances_):
+for feature, importance in zip(X_train.columns, model.feature_importances_):
     print(f"{feature}: {importance:.4f}")
 
 # now i have a dataframe fighters_name that is composed by the id and the names, our dataframe for the prediction and the response for each row. We are curious to see which are the prediction for each fight
 predictions = X_pred[["f_1", "f_2"]].copy()
-predictions["predicted_winner"] = tuned_model.predict(X_pred)
+predictions["predicted_winner"] = model.predict(X_pred)
 predictions["real_winner"] = y_pred.values
 
 predictions = predictions.merge(fighters_name.add_suffix('_1'), left_on=["f_1"], right_on=["fighter_id_1"])
